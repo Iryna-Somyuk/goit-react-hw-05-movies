348fe40a8720f4218f824446d8ab291b696f9907
Iryna-Somyuk <107417578+Iryna-Somyuk@users.noreply.github.com>
2023-02-25 14:55:49 +0000

    Deploying to gh-pages from @ Iryna-Somyuk/goit-react-hw-05-movies@42d870466c26525988812e7d1e5b22047b66285e 🚀

---
+++ static/js/186.306e5089.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Reviews"})}}}]);
+//# sourceMappingURL=186.306e5089.chunk.js.map
